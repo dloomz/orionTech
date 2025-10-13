65e7e20170420f01d753c848d7eae939e79ef827
dloomz <dolapo20okuboyejo@gmail.com>
2025-10-14 00:18:30 +0100

    Add OrionUtils class and configuration management; create initial files and directories

---
+++ startup/multifiles.py
@@ -0,0 +1,14 @@
+from utils.orionUtils import OrionUtils
+import os
+
+orion = OrionUtils()
+pref_path = "O:\\00_pipeline\\userPrefs"
+
+users = os.listdir(pref_path)
+software = orion.software
+
+for u in users:
+    for s in software:
+        outputs = os.path.join(pref_path, u, s)
+        os.makedirs(outputs)
+        print(outputs)
--- startup/utils/orionUtils.py
+++ startup/utils/orionUtils.py
@@ -8,21 +8,16 @@ class OrionUtils():
          self.location = self.is_at_home()
          self.root_dir = self.get_root_dir()
          self.json_path = self.get_json_path()
+         self.usernames = self.read_config("usernames")
+         self.software = self.read_config("software")
          
     def is_at_home(self):
         
-        user = os.getlogin()
-        
-        at_home = None
-        if user == "do23aaf":
-            at_home = False
-        else: 
-            at_home = True
-        return at_home
+        return os.getlogin() != "do23aaf"
     
     def get_root_dir(self):
          
-        if self.location == False:
+        if self.location == True:
             root_dir =  "O:\\"
         else:
             root_dir = "P:\\all_work\\studentGroups\\ORION_CORPORATION"  
@@ -37,6 +32,16 @@ class OrionUtils():
     def read_json(self, file):
        
         with open(file) as f:
+            
             data = json.load(f)
             f.close()
+            
         return data
+    
+    def read_config(self, key):
+        
+        config_path = os.path.join(self.json_path, "config.json")
+        config_data = self.read_json(config_path)
+        
+        return config_data.get(key, None)
+        
