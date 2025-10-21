d7dfc01f6347620f7927ebabe8eec6d9c8d956fd
dloomz <dolapo20okuboyejo@gmail.com>
2025-10-22 00:30:39 +0100

    Initialize SystemUtils and set wallpaper on startup

--- startup/main.py
+++ startup/main.py
@@ -1,8 +1,14 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 from ui.orionTechUI import OrionTechUI
+from utils.systemUtils import SystemUtils
+
+system_utils = SystemUtils()
 
 if __name__ == '__main__':
+    
+    system_utils.change_wallpaper("graphics\orionBack_001.png")
+    
     app = QApplication(sys.argv)
     ex = OrionTechUI()
     ex.show()
